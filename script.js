2d6c5f0763a3db824edb8ff06dba22cb6b553062
Adriano Dias <adriano.rj.br@gmail.com>
2022-12-09 18:19:20 -0300

    inclusao tarefa 2 com refatoracoes

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-async function listPlanets() {
-  const planets = await fetch('https://swapi.dev/api/planets/?format=json');
-  let { results } = await planets.json();
+let fetchedPlanets;
 
-  console.log(results);
+async function getPlanets() {
+  if (fetchedPlanets == undefined) {
+    const planets = await fetch('https://swapi.dev/api/planets/?format=json');
+    fetchedPlanets = await planets.json();
+  }
+  return fetchedPlanets.results;
 }
 
-listPlanets();
+async function printPlanetListToConsole() {
+  console.log(await getPlanets());
+}
+
+async function createPlanetButtons() {
+  const planets = await getPlanets();
+  planets.forEach((planet) => {
+    let button = document.createElement('button');
+    button.innerText = planet.name;
+    document.getElementById('planets-menu').appendChild(button);
+  });
+}
+
+async function runner() {
+  await printPlanetListToConsole(); // atividade 1
+  await createPlanetButtons(); // atividade 2
+}
+
+runner();
